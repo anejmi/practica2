237b1057ea5f25e3bab3201f63a33fe1a4b081f0
anejmi <57992965+anejmi@users.noreply.github.com>
2020-12-15 20:05:19 -0300

    commit1

---
+++ crud-practica2/src/main/java/com/example/demo/Practica2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2.class, args);
+	}
+
+}
